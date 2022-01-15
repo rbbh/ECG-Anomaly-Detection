cc560eeea7672f2da422ce077aa51215f9166423
rbbh <holandaraf@gmail.com>
2022-01-15 17:00:40 -0300

    Removing batch normalization.

--- source/autoencoder.py
+++ source/autoencoder.py
@@ -17,20 +17,18 @@ class AutoEncoder(nn.Module):
             nn.MaxPool2d(kernel_size=(2, 2)),
 
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding="same"),
-            nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=(2, 2)),
 
             nn.Dropout(0.2)
         )
 
-        self.linear_1 = nn.Linear(8 * 8 * 64, dense_neurons)
-        self.linear_2 = nn.Linear(dense_neurons, 8 * 8 * 64)
+        self.linear_1 = nn.Linear(8 * 8 * 64, self.__dense_neurons)
+        self.linear_2 = nn.Linear(self.__dense_neurons, 8 * 8 * 64)
 
         self.decoder = nn.Sequential(
             nn.Upsample(scale_factor=(2, 2)),
             nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3, 3), padding="same"),
-            nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
 
             nn.Upsample(scale_factor=(2, 2)),
