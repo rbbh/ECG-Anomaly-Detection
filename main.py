3d1aacf12b005bb66a67d5fe820acb5cd84210d8
rbbh <holandaraf@gmail.com>
2022-01-18 20:14:38 -0300

    Upgrading main.py.

--- main.py
+++ main.py
@@ -27,11 +27,16 @@ def get_config_parser():
 
 
 def preprocess_pipeline(parser, dataset_obj):
-    mit_dir = parser["SIGNALS"]["mit_dir"]
-    channel = int(parser["SIGNALS"]["channel"])
 
+    feature_type = parser["PREPROCESS"]["feature_type"]
+    pickle_name = parser["PREPROCESS"]["pickle_name"]
+
+    samples = int(parser["PREPROCESS"]["signal_sample_amount"])
+    window_len = int(parser["PREPROCESS"]["window_len"])
+    step_len = int(parser["PREPROCESS"]["step_len"])
+
+    window_type = parser["PREPROCESS"]["window"]
     wavelet_name = parser["PREPROCESS"]["wavelet"]
-    pickle_path = parser["PREPROCESS"]["pickle_path"]
 
     val_split_pct = float(parser["PREPROCESS"]["train_val_split_pct"])
     batch_size = int(parser['DL-MODEL']['batch_size'])
@@ -41,25 +46,22 @@ def preprocess_pipeline(parser, dataset_obj):
     annotations = dataset_obj.get_annotations
     peaks = dataset_obj.get_peak_locations
 
-    preprocess_obj = Preprocess(signals, annotations, peaks, val_split_pct, wavelet_name, channel, mit_dir, pickle_path)
-    normal_scalograms = preprocess_obj.get_normal_scalograms
-    abnormal_scalograms = preprocess_obj.get_abnormal_scalograms
+    preprocess_obj = Preprocess(signals, annotations, peaks, val_split_pct, feature_type, pickle_name, samples,
+                                window_len, window_type, step_len, wavelet_name)
+    normal_features = preprocess_obj.get_normal_2d_features
+    abnormal_features = preprocess_obj.get_abnormal_2d_features
 
-    normal_scalograms = preprocess_obj.normalize(normal_scalograms)
-    abnormal_scalograms = preprocess_obj.normalize(abnormal_scalograms)
-    train_data, val_data, normal_test_data = preprocess_obj.shuffle_and_split_dataset(normal_scalograms)
-    test_data = preprocess_obj.join_datasets(normal_test_data, abnormal_scalograms)
+    normal_features = preprocess_obj.normalize(normal_features)
+    abnormal_features = preprocess_obj.normalize(abnormal_features)
+
+    train_data, val_data, normal_test_data = preprocess_obj.shuffle_and_split_dataset(normal_features)
+    test_data = preprocess_obj.join_datasets(normal_test_data, abnormal_features)
 
     train_torch_ds = preprocess_obj.to_torch_ds(train_data, batch_size)
     val_torch_ds = preprocess_obj.to_torch_ds(val_data, val_batch_size)
     test_torch_ds = preprocess_obj.to_torch_ds(test_data, test=True)
 
-    return train_torch_ds, val_torch_ds, test_torch_ds
-
-
-def load_model_weights(model_obj, path):
-    checkpoint = torch.load(path)
-    model_obj.load_state_dict(checkpoint['model_state_dict'])
+    return train_torch_ds, val_torch_ds, test_torch_ds, preprocess_obj
 
 
 def run(parser):
@@ -74,20 +76,21 @@ def run(parser):
     ml_model = parser['ML-MODEL']['model']
 
     dataset_obj = MITBIH(mit_dir, channel)
-    train_loader, val_loader, test_loader = preprocess_pipeline(parser, dataset_obj)
+    train_loader, val_loader, test_loader, preprocess_obj = preprocess_pipeline(parser, dataset_obj)
 
     model = AutoEncoder(in_channels=1, dense_neurons=dense_neurons).to(device)
     input_shape = train_loader.dataset[0][0].shape
     summary(model, input_shape)
 
     if not weights_path:
-        train_obj = Trainer(model, train_loader, val_loader, epochs, learning_rate, checkpoint_pct, device)
+        train_obj = Trainer(model, preprocess_obj, train_loader, val_loader, epochs, learning_rate, checkpoint_pct, device)
         final_model_path = train_obj.train_autoencoder()
         weights_path = Path(final_model_path)
 
-    load_model_weights(model, weights_path)
+    checkpoint = torch.load(weights_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
 
-    test_obj = Tester(test_loader, ml_model, device)
+    test_obj = Tester(test_loader, ml_model, preprocess_obj, device)
     test_obj.evaluate(model)
 
 
