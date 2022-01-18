b0bb8a00fa4e31d83067181a2d14461046e16878
rbbh <holandaraf@gmail.com>
2022-01-18 20:09:41 -0300

    Documented metrics.py.

--- source/metrics.py
+++ source/metrics.py
@@ -6,6 +6,56 @@ from sklearn.decomposition import PCA
 
 
 class Metrics:
+    """This class computes the metrics resulted from the inference on the signals.
+
+    Attributes
+    ----------
+    _Metrics.__tp : int
+                    Number of true positive values.
+
+    _Metrics.__tn : int
+                    Number of true negative values.
+
+    _Metrics.__fp : int
+                    Number of false positive values.
+
+    _Metrics.__fn : int
+                    Number of false negative values.
+
+    _Metrics.__normal_vectors : list
+                                Normal flattened feature vectors.
+
+    _Metrics.__abnormal_vectors : list
+                                  Abnormal flattened feature vectors.
+
+    _Metrics.__feature_type : str
+                              Type of feature used.
+
+    _Metrics.__precision : float
+                           Precision metric value.
+
+    _Metrics.__recall : float
+                        Recall metric value.
+    
+
+    Methods
+    -------
+    _Metrics.compute_accuracy : Public
+                                Computes the Accuracy metric.
+
+    _Metrics.compute_precision : Public
+                                 Computes the Precision metric.
+
+    _Metrics.compute_recall : Public
+                              Computes the Recall metric.
+
+    _Metrics.compute_f1_score : Public
+                                Computes the F1-Score metric.
+
+    _Metrics.__plot_feature_vectors : Private
+                                      Plots the flattened feature vectors in a 2 dimensional space.
+
+    """
 
     def __init__(self, tp, tn, fp, fn, normal_vectors, abnormal_vectors, feature_type):
         self.__tp = tp
@@ -16,23 +66,66 @@ class Metrics:
         self.__abnormal_vectors = abnormal_vectors
         self.__feature_type = feature_type
 
-        self.__precision = self.compute_recall()
+        self.__precision = self.compute_precision()
         self.__recall = self.compute_recall()
         self.__plot_feature_vectors()
 
     def compute_accuracy(self):
+        """Computes the Accuracy metric.
+
+        Returns
+        -------
+        accuracy : float
+                   TP + TN
+                  ---------
+               TP + TN + FP + FN
+        """
         return (self.__tp + self.__tn) / (self.__tp + self.__tn + self.__fp + self.__fn)
 
     def compute_precision(self):
+        """Computes the Precision metric.
+
+        Returns
+        -------
+        precision : float
+                    TP
+                  -------
+                  TP + FP
+        """
         return self.__tp / (self.__tp + self.__fp)
 
     def compute_recall(self):
+        """Computes the Recall metric.
+
+        Returns
+        -------
+        recall : float
+                 TP
+               -------
+               TP + FN
+        """
         return self.__tp / (self.__tp + self.__fn)
 
     def compute_f1_score(self):
+        """Computes the F1-Score metric.
+
+        Returns
+        -------
+        f1_score : float
+                   2 x precision x recall
+                  -----------------------
+                    precision + recall
+        """
         return 2 * self.__precision * self.__recall / (self.__precision + self.__recall)
 
     def __plot_feature_vectors(self):
+        """Plots the flattened feature vectors in a 2D space after performing dimensionality reductions.
+
+        Returns
+        -------
+        None
+
+        """
         base_path = Path(f"outputs/plots/{self.__feature_type}")
         concatenated_vectors = np.concatenate((self.__normal_vectors, self.__abnormal_vectors), axis=0)
         tsne = TSNE(n_components=2, verbose=0, n_jobs=-1)
